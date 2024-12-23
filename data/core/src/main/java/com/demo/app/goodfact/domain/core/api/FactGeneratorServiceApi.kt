e23fd4f0ded65840169c76c79809fa132ba8a406
romeljordan <romeljordan25@gmail.com>
2024-12-23 10:30:56 +0800

    Add fact generator API

---
+++ data/core/src/main/java/com/demo/app/goodfact/domain/core/api/FactGeneratorServiceApi.kt
@@ -0,0 +1,14 @@
+package com.demo.app.goodfact.domain.core.api
+
+import com.demo.app.goodfact.domain.core.dto.FactDto
+import retrofit2.Response
+import retrofit2.http.GET
+
+
+interface FactGeneratorServiceApi {
+    @GET("api/v2/facts/random")
+    suspend fun getRandomFact(): Response<FactDto>
+
+    @GET("api/v2/facts/today")
+    suspend fun getDailyFact(): Response<FactDto>
+}
--- data/core/src/main/java/com/demo/app/goodfact/domain/core/datasource/FactGeneratorRemoteDataSource.kt
+++ data/core/src/main/java/com/demo/app/goodfact/domain/core/datasource/FactGeneratorRemoteDataSource.kt
@@ -1,14 +1,14 @@
 package com.demo.app.goodfact.domain.core.datasource
 
+import com.demo.app.goodfact.domain.core.api.FactGeneratorServiceApi
 import com.demo.app.goodfact.domain.core.dto.FactDto
 import retrofit2.Response
+import javax.inject.Inject
 
-final class FactGeneratorRemoteDataSource {
-    suspend fun generateRandomFact(): Response<FactDto> {
-        TODO("")
-    }
+final class FactGeneratorRemoteDataSource @Inject constructor(
+    private val apiService: FactGeneratorServiceApi
+) {
+    suspend fun generateRandomFact(): Response<FactDto> = apiService.getRandomFact()
 
-    suspend fun generateDailyFact(): Response<FactDto> {
-        TODO("")
-    }
+    suspend fun generateDailyFact(): Response<FactDto> = apiService.getDailyFact()
 }
