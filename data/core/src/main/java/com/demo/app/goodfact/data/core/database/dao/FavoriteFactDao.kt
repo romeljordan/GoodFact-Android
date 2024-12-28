e5573e3b740639c2888c9f1e4752f88a07c36d2e
romeljordan <romeljordan25@gmail.com>
2024-12-28 20:51:42 +0800

    Update FavoriteFactDao.kt

--- data/core/src/main/java/com/demo/app/goodfact/data/core/database/dao/FavoriteFactDao.kt
+++ data/core/src/main/java/com/demo/app/goodfact/data/core/database/dao/FavoriteFactDao.kt
@@ -5,12 +5,16 @@ import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.demo.app.goodfact.domain.core.model.Fact
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface FavoriteFactDao {
     @Insert(onConflict = OnConflictStrategy.IGNORE)
-    fun insert(fact: Fact)
+    suspend fun insert(fact: Fact)
 
     @Query(value = "DELETE FROM FavoriteFact WHERE id=:id")
-    fun removeFavorite(id: String)
+    suspend fun removeFavorite(id: String)
+
+    @Query(value = "SELECT * FROM FavoriteFact")
+    suspend fun fetchFavorites(): Flow<List<Fact>>
 }
