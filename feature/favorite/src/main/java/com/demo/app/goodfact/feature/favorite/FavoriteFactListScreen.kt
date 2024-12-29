fa2b4a2661d7145091170b21a56c0cde3a16bed3
romeljordan <romeljordan25@gmail.com>
2024-12-29 19:54:29 +0800

    Add row title uis

--- feature/favorite/src/main/java/com/demo/app/goodfact/feature/favorite/FavoriteFactListScreen.kt
+++ feature/favorite/src/main/java/com/demo/app/goodfact/feature/favorite/FavoriteFactListScreen.kt
@@ -2,10 +2,12 @@ package com.demo.app.goodfact.feature.favorite
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -15,9 +17,11 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.demo.app.goodfact.domain.core.model.Fact
@@ -47,18 +51,28 @@ private fun FavoriteFactListScreen(
         modifier = Modifier.fillMaxSize(),
         containerColor = AppColor.ghostWhite
     ) { innerPadding ->
-        Box(
+        Column(
             modifier = Modifier
                 .padding(innerPadding)
-                .fillMaxSize(),
-            contentAlignment = Alignment.Center
+                .fillMaxSize()
         ) {
+            Text(
+                modifier = Modifier.padding(start = 16.dp),
+                text = "Favorites",
+                fontSize = 30.sp,
+                fontWeight = FontWeight.Bold,
+                color = Color.Black
+            )
+
             if (favorites.isEmpty()) {
                 Text(
-                    text = "No saved fact yet.",
+                    text = "No favorite facts yet.",
                     textAlign = TextAlign.Center,
                     fontStyle = FontStyle.Italic,
-                    color = Color.Gray
+                    color = Color.Gray,
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .wrapContentHeight(Alignment.CenterVertically)
                 )
             } else {
                 LazyColumn(
